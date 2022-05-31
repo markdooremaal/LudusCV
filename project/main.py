38adff47dbbcd4b3ead9d9ce2b031c38815b681c
J0113 <jollesnaas1@gmail.com>
2022-05-31 14:31:54 +0200

    Arm detection

--- project/main.py
+++ project/main.py
@@ -1,23 +1,18 @@
 from NeuralNetwork import NeuralNetwork
+from ArmDetection import ArmDetection
 import cv2
 
-colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
-
 def main():
-    nn = NeuralNetwork("../models/30052022-start-pos.onnx", ["startingpos"])
+    nn = NeuralNetwork("../models/24052022.onnx", ["startingpos", "light"])
+    arm_detect = ArmDetection()
+    capture = cv2.VideoCapture("../experiments/example_data/lampjetest.mp4")
 
-    capture = cv2.VideoCapture(0)
     while True:
         _, frame = capture.read()
 
-        class_ids, confidences, boxes = nn.detect_on_frame(frame, True)
-
-        for (classid, confidence, box) in zip(class_ids, confidences, boxes):
-            color = colors[int(classid) % len(colors)]
-            cv2.rectangle(frame, box, color, 2)
-            cv2.rectangle(frame, (box[0], box[1] - 20),
-                          (box[0] + box[2], box[1]), color, -1)
-            cv2.putText(frame, "{0} - {1:.2f}".format(nn.class_list[classid], confidences[int(confidence)]), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
+        # frame = arm_detect.find_pose(frame)
+        lmList, bboxInfo = arm_detect.find_position(frame)
+        frame = nn.draw_on_frame(frame)
 
         cv2.imshow("output", frame)
 
