9e51df322bebb2bf792cd9537ddff3b841303ad2
J0113 <jollesnaas1@gmail.com>
2022-05-31 14:31:49 +0200

    draw_on_frame

--- project/NeuralNetwork.py
+++ project/NeuralNetwork.py
@@ -89,3 +89,16 @@ class NeuralNetwork:
         input_image = self.format_yolov5(frame)
         outs = self.detect(input_image, self.network)
         return self.wrap_detection(input_image, outs[0], low_confidence)
+
+    def draw_on_frame(self, frame, low_confidence = False):
+        class_ids, confidences, boxes = self.detect_on_frame(frame, low_confidence)
+        colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
+
+        for (classid, confidence, box) in zip(class_ids, confidences, boxes):
+            color = colors[int(classid) % len(colors)]
+            cv2.rectangle(frame, box, color, 2)
+            cv2.rectangle(frame, (box[0], box[1] - 20),
+                          (box[0] + box[2], box[1]), color, -1)
+            cv2.putText(frame, "{0} - {1:.2f}".format(self.class_list[classid], confidences[int(confidence)]), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
+
+        return frame
